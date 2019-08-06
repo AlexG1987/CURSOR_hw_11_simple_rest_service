7729b412253dd774de6c423c413e021393d5e6bb
Гаврилюк Олександр Михайлович <o.gavrilyuk@wr.int>
2019-08-06 11:32:10 +0300

    Some fixes in Controller

--- src/main/java/com/cursor/spring/hw_11_simple_rest_service/controller/LibraryController.java
+++ src/main/java/com/cursor/spring/hw_11_simple_rest_service/controller/LibraryController.java
@@ -3,6 +3,7 @@ package com.cursor.spring.hw_11_simple_rest_service.controller;
 import com.cursor.spring.hw_11_simple_rest_service.entity.Author;
 import com.cursor.spring.hw_11_simple_rest_service.entity.Book;
 import com.cursor.spring.hw_11_simple_rest_service.service.LibraryServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +13,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/library")
+@AllArgsConstructor
 public class LibraryController {
 
-    final
-    LibraryServiceImpl LibraryServiceImpl;
-
-    public LibraryController(LibraryServiceImpl LibraryServiceImpl) {
-        this.LibraryServiceImpl = LibraryServiceImpl;
-    }
+    @Autowired
+    final LibraryServiceImpl LibraryServiceImpl;
 
     @PostMapping("/addAuthor")
     public List<Author> addAuthor(@RequestBody Author author) {
@@ -50,25 +48,21 @@ public class LibraryController {
         return LibraryServiceImpl.getBooksByGenre(genre);
     }
 
-    @ResponseBody
     @DeleteMapping("/deleteAuthor/{id}")
     public List<Author> deleteAuthor(@PathVariable("id") int authorId) {
         return LibraryServiceImpl.deleteAuthor(authorId);
     }
 
-    @ResponseBody
     @DeleteMapping("/deleteBook/{id}")
     public List<Book> deleteBook(@PathVariable("id") int bookId) {
         return LibraryServiceImpl.deleteBook(bookId);
     }
 
-    @ResponseBody
     @PatchMapping("updateAuthor/{id}")
     public List<Author> updateAuthor(@PathVariable("id") int oldAuthorId, @RequestBody Author author) {
         return LibraryServiceImpl.updateAuthor(oldAuthorId, author);
     }
 
-    @ResponseBody
     @PatchMapping("updateBook/{id}")
     public List<Book> updateBook(@PathVariable("id") int oldBookId, @RequestBody Book book) {
         return LibraryServiceImpl.updateBook(oldBookId, book);
